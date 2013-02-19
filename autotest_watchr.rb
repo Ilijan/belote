5fa65c7f41d9640a32cc57e1f3998b53c6d6c728
Ilijan <ilianvel@abv.bg>
2013-02-19 22:31:37 +0200

    Added autotest feature with watchr

---
+++ autotest_watchr.rb
@@ -0,0 +1,13 @@
+system 'cls'
+
+watch('test/(.*)_tests.rb') do |md| 
+  system 'cls'
+  #system "ruby -w #{md[0]}"
+  system "rspec -r '.\\lib\\#{md[1]}.rb' #{md[0]}"
+end
+
+watch('lib/(.*).rb') do |md|
+  system 'cls'
+  system "ruby -w #{md[0]}"
+  system "rspec -r '.\\#{md[0]}' .\\test\\#{md[1]}_tests.rb"
+end
