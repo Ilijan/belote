14e97863d9fbfa10b8186e83df7f2ecfe53b8d6e
Ilijan <ilianvel@abv.bg>
2013-05-11 02:48:04 +0300

    Now autotest script displays the name of file that was changed.

--- autotest_watchr.rb
+++ autotest_watchr.rb
@@ -2,12 +2,14 @@ system 'cls'
 
 watch('test/(.*)_tests.rb') do |md| 
   system 'cls'
+  system "echo Modified #{md[0]}"
   #system "ruby -w #{md[0]}"
   system "rspec -r '.\\lib\\#{md[1]}.rb' #{md[0]}"
 end
 
 watch('lib/(.*).rb') do |md|
   system 'cls'
+  system "echo Modified #{md[0]}"
   system "ruby -w #{md[0]}"
   system "rspec -r '.\\#{md[0]}' .\\test\\#{md[1]}_tests.rb"
 end
